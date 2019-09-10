e2a67d9b847ef5082408cdedcbbef9d4724d2d94
Gelbpunkt <jens@troet.org>
2019-09-10 16:40:19 +0000

    Finalization

--- routes/api/genadventures.js
+++ routes/api/genadventures.js
@@ -26,14 +26,16 @@ router.post("/", async (req, res) => {
       .send({ err: `percentages is a required argument and is missing` });
   }
   var out = [];
+  console.log(req.body.percentages);
   images.forEach(function(image, index) {
     const canvas = createCanvas(425, 220, "png");
     const ctx = canvas.getContext("2d");
     const percents = req.body.percentages[index];
+    console.log(percents);
     ctx.drawImage(image, 0, 0);
-    ctx.font = "24px TravMedium, CaviarDreams, OpenSansEmoji";
-    ctx.fillText(`${percents[0]}% to`, 314, 168, 100);
-    ctx.fillText(`${percents[1]}%`, 314, 195, 100);
+    ctx.font = "20px TravMedium, CaviarDreams, OpenSansEmoji";
+    ctx.fillText(`${percents[0]}% to`, 314, 185, 100);
+    ctx.fillText(`${percents[1]}%`, 314, 205, 100);
     const buffer = canvas.toDataURL();
     out.push(buffer);
   });
