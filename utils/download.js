f9311b5e92fbbc2c3bf903257613d59f3e325c5c
Jens Reidel <jens@troet.org>
2020-03-28 22:38:47 +0100

    ignore 400s

--- utils/download.js
+++ utils/download.js
@@ -4,21 +4,19 @@ const HttpsProxyAgent = require("https-proxy-agent");
 const proxy = require("../config.json").proxy;
 const proxy_auth = require("../config.json").proxy_auth;
 
-
 async function fetch(url) {
   var headers = {
     "Proxy-Authorization-Key": proxy_auth,
     "Requested-URI": url,
     "Accept": "application/json"
   }
-  return await nodeFetch(proxy, { timeout: 5* 1000, size: 4000000, headers: headers });
+  return await nodeFetch(proxy, { timeout: 5 * 1000, size: 4000000, headers: headers });
 }
 
 
 async function getData(url) {
   try {
     const response = await fetch(url);
-    if (!response.ok) throw Error(`The server returned ${response.status}`);
     return await response.buffer();
   } catch (error) {
     throw error;
